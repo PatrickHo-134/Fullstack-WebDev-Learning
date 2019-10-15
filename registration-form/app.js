eefefcde718d808ab3dca1b39a53aac50afb59fb
Phat Vinh Ho <patt.hvp@gmail.com>
2019-10-15 15:18:44 +1100

    reallocate local files

--- registration-form/app.js
+++ registration-form/app.js
@@ -6,22 +6,27 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// connect to the database
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/collect-info");
 
+// create a database schema
 var nameSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   email: String
 });
 
+// create a model in the database
 var User = mongoose.model("User", nameSchema);
 
+// use sendFile command to show the index.html file
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// build a Create-Read-Update-Delete endpoint
 app.post("/addname", (req, res) => {
   var myData = new User(req.body);
   myData
