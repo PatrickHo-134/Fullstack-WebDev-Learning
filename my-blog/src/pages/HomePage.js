1e668ddfbf27e857aec40a2e33aea2d28e939d35
Phat Vinh Ho <patt.hvp@gmail.com>
2019-10-17 15:34:51 +1100

    add 2 new projects

---
+++ my-blog/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = () => (
+  // wrap our content in a react.fragment instead of <div>
+  <React.Fragment>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+      This is the blog I create for the purpose of learning web development
+      using React.js, Node.js and MongoDB
+    </p>
+  </React.Fragment>
+);
+
+export default HomePage;
