1c0c891bb38cfbb2818de5fd4866f9f667544db8
Phat Vinh Ho <patt.hvp@gmail.com>
2020-01-20 17:15:36 +1100

    react firebase project

---
+++ react-firebase-authentication/src/components/Account/index.js
@@ -0,0 +1,37 @@
+// !!!previous version
+// import React from "react";
+// import { PasswordForgetForm } from "../PasswordForget";
+// import PasswordChangeForm from "../PasswordChange";
+
+// const AccountPage = () => (
+//   <div>
+//     <h1>Account Page</h1>
+//     <PasswordForgetForm />
+//     <PasswordChangeForm />
+//   </div>
+// );
+
+// export default AccountPage;
+
+import React from "react";
+
+import { AuthUserContext } from "../Session";
+import { PasswordForgetForm } from "../PasswordForget";
+import PasswordChangeForm from "../PasswordChange";
+import { withAuthorization } from "../Session";
+
+const AccountPage = () => (
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
+);
+
+const authCondition = authUser => !!authUser;
+
+export default withAuthorization(authCondition)(AccountPage);
---
+++ react-firebase-authentication/src/components/Navigation/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import SignOutButton from "../SignOut";
+import * as ROUTES from "../../constants/routes";
+import { AuthUserContext } from "../Session";
+
+// The Navigation component can be made aware of authenticated user to display different options.
+// It should either show the available links for an authenticated user or a non authenticated user.
+const Navigation = () => (
+  <div>
+    <AuthUserContext.Consumer>
+      {authUser => (authUser ? <NavigationAuth /> : <NavigationNonAuth />)}
+    </AuthUserContext.Consumer>
+  </div>
+);
+
+const NavigationAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.HOME}>Home</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ACCOUNT}>Account</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.ADMIN}>Admin</Link>
+    </li>
+    <li>
+      <SignOutButton />
+    </li>
+  </ul>
+);
+
+const NavigationNonAuth = () => (
+  <ul>
+    <li>
+      <Link to={ROUTES.LANDING}>Landing</Link>
+    </li>
+    <li>
+      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+    </li>
+  </ul>
+);
+
+export default Navigation;
